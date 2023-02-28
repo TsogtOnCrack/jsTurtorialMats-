60e1a2bf1620efba7a1b855ec60d0e5871e3a143
Tsogt Otgonsuren <ts0gt@Otgonsurens-MacBook-Pro.local>
2023-02-28 16:01:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+
+// // declaring vars
+// console.log("Hello world") // print()    
+
+// const a = 4
+
+// console.log(a);
+
+// // conditions
+
+// //condition
+// if(a == 3){ // if
+//     console.log("hello")
+// }else{
+//     console.log("not 3")
+// }
+
+// let i = 0;
+// let n = 10
+// while(i < n){
+//     console.log(i);
+//     i ++ // i+= // i = i + 1
+// }
+
+// //loops
+
+// // for i in Range(32):
+// //     efwefwfw
+// //     wefwefwef
+// //     ewf
+
+// for (let i = 0; i < n; i ++){
+//     console.log(i, "this is for loop part")
+// }
+
+const MyArray = [
+    232,342,34,23,42,342,34,234,3, 2342342, 10
+]
+
+//map
+// let sum = 0
+// MyArray.map((e, i)=>{
+//     console.log(i, e)
+// })
+
+
+// for (let i = 0; i < MyArray.length; i ++){//for loop that will go throught the array
+//     console.log(MyArray[i])
+// }
+
+
+//FUNCTIONS 
+
+// function Myfunc(x){
+//     console.log(x + 2)
+// }
+// Myfunc(4)
+
+
+// const Myfunc = (x)=>{
+//     console.log(x + 3)
+// }
+
+// let x = 30
+// Myfunc(x)
+
+// let ar = []
+
+// for (let i = 0; i < 10; i ++){
+//     let arrrr = []
+
+//     for (let i=0; i < 10; i ++){
+//         arrrr.push(" LOL ")
+//     }
+
+//     ar.push(arrrr)
+// }
+
+// console.log(ar)
+
---
+++ it.py
@@ -0,0 +1,10 @@
+ar = []
+
+for i in range(10):
+    arrr = []
+    for i in range(10):
+        arrr.append(0)
+    ar.append(arrr)
+    
+for el in ar:
+    print(el)
